4f91f7bb2bce61ea1542671fa48ef1577bc1bf51
griminside <grimbehindspects@gmail.com>
2022-09-05 12:57:54 +0530

    1873

--- 1873.calculate-special-bonus.sql
+++ 1873.calculate-special-bonus.sql
@@ -7,5 +7,9 @@
 -- @lc code=start
 # Write your MySQL query statement below
 
+SELECT employee_id , IF (employee_id % 2 = 1 AND name NOT LIKE ('M%') ,salary , 0) AS bonus 
+FROM Employees
+ORDER BY employee_id;
+
 -- @lc code=end
 
