b641679a274cfbcd53e4ab1048d3989ac5c13af7
griminside <grimbehindspects@gmail.com>
2022-09-05 12:56:19 +0530

    196

--- 196.delete-duplicate-emails.sql
+++ 196.delete-duplicate-emails.sql
@@ -8,5 +8,8 @@
 # Please write a DELETE statement and DO NOT write a SELECT statement.
 # Write your MySQL query statement below
 
+DELETE p1 FROM Person p1 , Person p2
+WHERE p1.id > p2.id AND p1.email=p2.email; 
+
 -- @lc code=end
 
