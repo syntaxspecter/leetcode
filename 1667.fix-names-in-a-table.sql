ffeacb31aafdb4884e81ec9d16fda01b8edd3328
griminside <grimbehindspects@gmail.com>
2022-09-05 12:55:33 +0530

    1667

--- 1667.fix-names-in-a-table.sql
+++ 1667.fix-names-in-a-table.sql
@@ -7,5 +7,9 @@
 -- @lc code=start
 # Write your MySQL query statement below
 
+Select user_id , CONCAT( UPPER(Substring(name,1,1)) , LOWER(Substring(name , 2 , LENGTH(name)))) as name 
+From Users
+Order By user_id;
+
 -- @lc code=end
 
