886e31436daff15001dfc034b01812e0eb447823
griminside <grimbehindspects@gmail.com>
2022-09-05 12:51:51 +0530

    1527

---
+++ 1527.patients-with-a-condition.sql
@@ -0,0 +1,15 @@
+--
+-- @lc app=leetcode id=1527 lang=mysql
+--
+-- [1527] Patients With a Condition
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+
+SELECT patient_id , patient_name , conditions
+FROM Patients
+WHERE conditions LIKE ('% DIAB1%') OR conditions LIKE ('DIAB1%')
+
+-- @lc code=end
+
