bad9a63ba472e1ff6815e1cad0b2f92cbb463a4f
griminside <grimbehindspects@gmail.com>
2023-01-12 23:38:33 +0530

    2235

---
+++ 2235.add-two-integers.cpp
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=2235 lang=cpp
+ *
+ * [2235] Add Two Integers
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        return (num1+num2);
+        
+    }
+};
+// @lc code=end
+
