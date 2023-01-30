032f501af0bd9283494d88d66e919237f5a92603
griminside <grimbehindspects@gmail.com>
2023-01-30 23:23:05 +0530

    206

---
+++ 206.reverse-linked-list.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=206 lang=cpp
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        
+    }
+};
+// @lc code=end
+
